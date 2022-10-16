1bd0e7721f685c37149b224834329d854eafbc45
Tanzir Ratul <72658438+Tanzir-Ratul@users.noreply.github.com>
2022-10-16 18:14:28 +0600

    #day 3

--- lib/pages/employee_list_page.dart
+++ lib/pages/employee_list_page.dart
@@ -1,13 +1,11 @@
 import 'dart:io';
 
-import 'package:employee_management_app/models/employee_model.dart';
 import 'package:employee_management_app/providers/employee_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'employee_details_page.dart';
 import 'new_employee_page.dart';
-
 
 class EmployeeList extends StatefulWidget {
   static const routeName = "/";
@@ -19,7 +17,22 @@ class EmployeeList extends StatefulWidget {
 }
 
 class _EmployeeListState extends State<EmployeeList> {
-
+  Future<bool?> deleteConfirmation(DismissDirection direction) {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text("Delete"),
+              content: const Text("Are you sure ?"),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context, false),
+                    child: const Text("Cancel")),
+                ElevatedButton(
+                    onPressed: () => Navigator.pop(context, true),
+                    child: const Text("Yes")),
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,38 +41,56 @@ class _EmployeeListState extends State<EmployeeList> {
         title: const Text("Employee"),
       ),
       body: Consumer<EmployeeProvider>(
-        builder:(context,provider,child) => ListView.builder(
+        builder: (context, provider, child) => ListView.builder(
           itemCount: provider.employees.length,
-          itemBuilder:(context,index){
+          itemBuilder: (context, index) {
             final emp = provider.employees[index];
-            return ListTile(
-              onTap: () => Navigator.pushNamed(context, EmployeeDetails.routeName,
-                  arguments: emp),
-              leading: CircleAvatar(
-                backgroundImage: FileImage(File(emp.image)),
-              ),
-              title: Text(emp.name),
-              subtitle: Text(
-                emp.designation,
-              ),
-              trailing: IconButton(onPressed: (){
-                  provider.updateFavorite(index);
+            return Dismissible(
+              key: UniqueKey(),
+              direction: DismissDirection.endToStart,
+              confirmDismiss: deleteConfirmation,
+              onDismissed: (_){
+                provider.deleteEmployee(emp);
               },
-              icon: Icon(emp.favourite?Icons.favorite:Icons.favorite_border),),
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Icon(
+                  Icons.delete,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              ),
+              child: ListTile(
+                onTap: () => Navigator.pushNamed(
+                    context, EmployeeDetails.routeName,
+                    arguments: emp),
+                leading: CircleAvatar(
+                  backgroundImage: FileImage(File(emp.image)),
+                ),
+                title: Text(emp.name),
+                subtitle: Text(
+                  emp.designation,
+                ),
+                trailing: IconButton(
+                  onPressed: () {
+                    provider.updateFavorite(index);
+                  },
+                  icon: Icon(
+                      emp.favourite ? Icons.favorite : Icons.favorite_border),
+                ),
+              ),
             );
           },
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, NewEmployeePage.routeName);//.then((value) => setState((){}));
+          Navigator.pushNamed(context,
+              NewEmployeePage.routeName); //.then((value) => setState((){}));
         },
         child: const Icon(Icons.add),
       ),
     );
   }
-
-
-
-
 }
